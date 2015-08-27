5cfc527827950641f324546651de461dc406a8fd
truetamtam <truetamtam@yandex.ru>
2015-08-27 17:19:34 +0300

    Removing enc'ted fine.

--- e.sh
+++ e.sh
@@ -3,7 +3,7 @@
 
 . ./log.sh
 
-DATE=$(date +%H:%M)
+DATE=$(date +%d%m%Y)
 
 # required dirs
 if [[ ! -d bck ]]; then mkdir bck ; fi
@@ -17,9 +17,26 @@ HOLD_BCKS=3
 BCK_DIR='./bck'
 ENC_DIR='./enc'
 
+# we only need last 3 newest files in bck directory
+# sorted by modified time
+log "Checking for gzip's to remove"
+    REM_GZ_LST=$(ls -t $PWD/bck/* | awk 'NR>3')
+echo $REM_GZ_LST
+if [[ ! -z ${REM_GZ_LST// } ]]; then
+    echo "Removing..."
+    log "Removing..."
+    log "${REM_GZ_LST}"
+    rm -f ${REM_GZ_LST}
+else
+    echo 'Nothing to remove.'
+    log 'Nothing to remove.'
+fi
+#rm -f $(ls -t $PWD/bck/* | awk 'NR>3')
 
-# ls -p | grep -v /
-
+# getting latest archive to enc
+LATEST_GZIP=$(ls -t $PWD/bck/* | awk 'NR==1')
+#echo $LATEST_GZIP
 
 
 #openssl smime -encrypt -aes256 -in secret.txt -binary -outform DEM -out secret.txt.enc bckpub.pem
+
