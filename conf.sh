71cc3b4525f1ed7ea4c023bffb5109e5b422f1b8
truetamtam <truetamtam@yandex.ru>
2015-08-28 10:22:01 +0300

    Moved config to separate file.

---
+++ conf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# conf
+#
+# number of archives to store(newest stays)
+HOLD_BCKS=3
+# number of enc'ted archives to store(newest stays)
+HOLD_ENC=1
+# raw storage
+BCK_DIR='/d/www/encr_bck/bck'
+# enc storage
+ENC_DIR='/d/www/encr_bck/enc'
+#
+# fnoc
--- e.sh
+++ e.sh
@@ -4,21 +4,9 @@
 # inc
 . ./log.sh
 . ./reg.sh
+. ./conf.sh
 
 log "+++ Starting session"
-
-# conf
-#
-# number of archives to store(newest stays)
-HOLD_BCKS=3
-# number of enc'ted archives to store(newest stays)
-HOLD_ENC=1
-# raw storage
-BCK_DIR='/d/www/encr_bck/bck'
-# enc storage
-ENC_DIR='/d/www/encr_bck/enc'
-#
-# fnoc
 
 # required dirs
 if [[ ! -d ${BCK_DIR} ]]; then mkdir ${BCK_DIR} ; fi
